39ada52f7a20096c37690bf914cc1d4df6009c89
nicholsonja <nicholsonja@apsu.edu>
2022-11-05 13:07:07 -0500

    fix blank

--- model_textures/cp_textures.sh
+++ model_textures/cp_textures.sh
@@ -4,5 +4,5 @@ for f in *.png
 do
 	tex=`echo $f | awk -F_ '{print $1}' | sed 's/\\..*//'`
 
-	cp $f ../models/${tex}_box/materials/textures
+	echo cp $f ../models/${tex}_box/materials/textures
 done
