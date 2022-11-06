0ff90a279ca55cab7d44a4c5bd739a9692503111
nicholsonja <nicholsonja@apsu.edu>
2022-11-05 19:16:37 -0500

    pathlib

--- make_path_01.py
+++ make_path_01.py
@@ -1,31 +1,9 @@
 #!/usr/bin/env python
 
 import re
+from pathlib import *
 from sys import argv
 from math import pi
-
-
-INPUT_TEMPLATE_FILE = "path-template.xml"
-OUTPUT_WORLD_FILE = "path_world.xml"
-
-MODEL_TEMPLATE = """
-      <model name='MODEL-NAME'>
-        <scale>1 1 1</scale>
-        <pose>MODEL-X MODEL-Y 0 0 -0 MODEL-YAW</pose>
-        <velocity>0 0 0 0 -0 0</velocity>
-        <acceleration>0 -0 0 0 -0 0</acceleration>
-        <wrench>0 -0 0 0 -0 0</wrench>
-      </model>
-"""
-
-INCLUDE_TEMPLATE = """
-    <model name='MODEL-NAME'>
-      <static>1</static>
-      <include>
-        <uri>model:///home/user/worlds/models/line</uri>
-	  </include>
-    </model>
-"""
 
 
 def get_includes_and_models(num_cells):
---
+++ pathlib/__init__.py
@@ -0,0 +1,21 @@
+INPUT_TEMPLATE_FILE = "path-template.xml"
+OUTPUT_WORLD_FILE = "path_world.xml"
+
+MODEL_TEMPLATE = """
+      <model name='MODEL-NAME'>
+        <scale>1 1 1</scale>
+        <pose>MODEL-X MODEL-Y 0 0 -0 MODEL-YAW</pose>
+        <velocity>0 0 0 0 -0 0</velocity>
+        <acceleration>0 -0 0 0 -0 0</acceleration>
+        <wrench>0 -0 0 0 -0 0</wrench>
+      </model>
+"""
+
+INCLUDE_TEMPLATE = """
+    <model name='MODEL-NAME'>
+      <static>1</static>
+      <include>
+        <uri>model:///home/user/worlds/models/line</uri>
+	  </include>
+    </model>
+"""
